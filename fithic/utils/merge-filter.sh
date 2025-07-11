47e50d248298418ac026d5e180556aa4ee8109e7
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2025-07-11 17:29:59 +0900

    first commit

--- fithic/utils/merge-filter.sh
+++ fithic/utils/merge-filter.sh
@@ -17,4 +17,4 @@ script=$(cd $(dirname $0) && pwd)"/CombineNearbyInteraction.py"
 outdir=$(dirname "${outputFile}")
 mkdir -p $outdir
 zcat $inputFile | awk '{if(NR!=1){print $0}}'| awk -v q="$fdr" '{if($7<=q){print $0}}' | gzip > $outdir/fithic_subset.gz
-python3 $script -i $outdir/fithic_subset.gz -H 0 -r $resolution -o $outputFile
+$script -i $outdir/fithic_subset.gz -H 0 -r $resolution -o $outputFile
